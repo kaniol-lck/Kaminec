e6fcfa4dc149eeb8f4828e5d187831b3af18cf16
kaniol <375520360@qq.com>
2018-02-16 16:28:40 +0800

    repair stupid error in AssetIndex

--- Kaminec/core/json/AssetIndex.cpp
+++ Kaminec/core/json/AssetIndex.cpp
@@ -10,30 +10,30 @@ AssetIndex::AssetIndex(QVariant assetIndexVariant) :
 
 QString AssetIndex::id() const
 {
-	static QString mId = value(assetIndexVariant_, "id").toString();
+	QString mId = value(assetIndexVariant_, "id").toString();
 	return mId;
 }
 
 int AssetIndex::size() const
 {
-	static int mSize = value(assetIndexVariant_, "size").toInt();
+	int mSize = value(assetIndexVariant_, "size").toInt();
 	return mSize;
 }
 
 QString AssetIndex::sha1() const
 {
-	static QString mSha1 = value(assetIndexVariant_, "sha1").toString();
+	QString mSha1 = value(assetIndexVariant_, "sha1").toString();
 	return mSha1;
 }
 
 QUrl AssetIndex::url() const
 {
-	static QUrl mUrl = value(assetIndexVariant_, "url").toUrl();
+	QUrl mUrl = value(assetIndexVariant_, "url").toUrl();
 	return mUrl;
 }
 
 int AssetIndex::totalSize() const
 {
-	static int mTotalSize = value(assetIndexVariant_, "totalSize").toInt();
+	int mTotalSize = value(assetIndexVariant_, "totalSize").toInt();
 	return mTotalSize;
 }
